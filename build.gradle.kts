9a9bb968bbad86d6a4562c9ab5622b0319d8dff4
Redwarp <redwarp@gmail.com>
2021-01-08 20:52:59 +0100

    Update to gradle 6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,6 @@ plugins {
     id("com.github.ben-manes.versions") version "0.36.0" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        jcenter()
-        mavenCentral()
-    }
-}
-
 subprojects {
     apply {
         plugin("org.jlleitschuh.gradle.ktlint")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,12 @@
 rootProject.name = "gifdecoder"
 
+dependencyResolutionManagement {
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+    }
+}
+
 includeBuild("decoder")
 include(":android-drawable",":android-drawable-native", ":android", ":benchmark")
