360cf9f32add39345ff8c3cf585b665e2dc37bbd
Redwarp <redwarp@gmail.com>
2021-11-10 16:07:19 +0100

    Bump libraries

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,10 +1,10 @@
 object Versions {
-    const val AGP = "7.0.2"
+    const val AGP = "7.0.3"
     const val BENCHMARK = "1.0.0"
     const val COROUTINES = "1.5.2"
-    const val DOKKA = "1.5.30"
+    const val DOKKA = "1.5.31"
     const val KOTLIN = "1.5.31"
-    const val KTLINT = "0.42.1"
+    const val KTLINT = "0.43.0"
     const val KTLINT_GRADLE = "10.1.0"
     const val SPOTLESS = "5.14.2"
     const val VERSIONS = "0.36.0"
