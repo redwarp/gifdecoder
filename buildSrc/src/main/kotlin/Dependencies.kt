6669cf33c1763e2140248f58c82c5e59de8bcc2b
Redwarp <redwarp@gmail.com>
2021-11-16 22:48:00 +0100

    Kotlin 1.6.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val BENCHMARK = "1.0.0"
     const val COROUTINES = "1.5.2"
     const val DOKKA = "1.5.31"
-    const val KOTLIN = "1.5.31"
+    const val KOTLIN = "1.6.0"
     const val KTLINT = "0.43.0"
     const val KTLINT_GRADLE = "10.1.0"
     const val SPOTLESS = "5.14.2"
