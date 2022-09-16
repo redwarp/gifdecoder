bd032145d13c1512606c51f9eabd303c1e7570be
Redwarp <redwarp@gmail.com>
2022-09-16 18:09:19 +0200

    Bump libraries

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
     const val AGP = "7.3.0"
-    const val APP_COMPAT = "1.5.0"
+    const val APP_COMPAT = "1.5.1"
     const val BENCHMARK = "1.0.0"
     const val DOKKA = "1.7.10"
     const val KOTLIN = "1.7.10"
-    const val SPOTLESS = "6.10.0"
+    const val SPOTLESS = "6.11.0"
     const val VERSIONS = "0.1.16"
 }
